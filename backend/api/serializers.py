205a95a1876c1c41e58d4d966fcf0e20fa232152
Александр <lexer777@mail.ru>
2022-10-22 10:36:56 +0300

    v.2.1

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,7 +2,7 @@ from django.db import transaction
 from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework import serializers, status
+from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework.fields import SerializerMethodField
 from rest_framework.relations import PrimaryKeyRelatedField
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,14 +6,13 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
 from rest_framework import status, viewsets
-from rest_framework.decorators import api_view
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.status import HTTP_400_BAD_REQUEST
 
 from recipes.models import (Favorite, Ingredient, Recipe, IngredientRecipe,
-                            ShoppingCart, Tag, ShoppingCart)
+                            ShoppingCart, Tag)
 from users.models import Follow, User
 
 from .filters import IngredientFilter, RecipeFilter
