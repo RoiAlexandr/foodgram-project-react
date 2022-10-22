f74165bac224f12052ef5dac6e2f56e1de546338
Александр <lexer777@mail.ru>
2022-10-22 10:55:53 +0300

    v.2.3

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
-env_path = Path('../infra')/'.env'
+env_path = Path('../infra') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
